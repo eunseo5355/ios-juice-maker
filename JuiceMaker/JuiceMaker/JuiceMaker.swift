e49fd4d759f80e63b5bbfc2a4559da62ae156d3f
eunseo5355 <eunseo5355@naver.com>
2021-03-10 00:34:20 +0900

    [Feat] step 1 구현

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -6,4 +6,26 @@
 
 import Foundation
 
-/// 쥬스 메이커 타입 
+struct JuiceMaker {
+    var fruits: [FruitTypes: Fruit] = [
+        .strawberry: Fruit(.strawberry),
+        .banana: Fruit(.banana),
+        .kiwi: Fruit(.kiwi),
+        .mango: Fruit(.mango),
+        .pineapple: Fruit(.pineapple)
+    ]
+    
+    func makeJuice(juice: JuiceTypes) {
+        for (fruit, requiredAmount) in juice.getRecipe() {
+            if let fruitStock = fruits[fruit] {
+                if fruitStock.checkStock(requiredAmount) {
+                    fruitStock.subtractStoke(requiredAmount)
+                } else {
+                    print("재고가 모자라요.")
+                    return
+                }
+            }
+        }
+        print("\(juice.getName()) 나왔습니다! 맛있게 드세요!")
+    }
+}
