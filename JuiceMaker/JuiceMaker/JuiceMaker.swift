1bd5411acf17a13c4bf501c0161a49499c2aa7c0
kio <un19451024@gmail.com>
2021-03-10 21:44:37 +0900

    Refactor: Creater checkStockOfFruit(), Update removeStockOfFruit(), makeFruitJuice(), juiceOrderProcess()
    1. checkStockPfFruit() 생성- 재고만 확인하기 위해
    2. removeStockOfFruit(), makeFruitJuice(), juiceOrderProcess() 매개변수 열거형으로 변경

--- JuiceMaker/JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/JuiceMaker.swift
@@ -23,6 +23,7 @@ enum TypesOfFruit {
     case kiwi
     case pineapple
     case mango
+    
 }
 
 class JuiceMaker {
@@ -32,16 +33,9 @@ class JuiceMaker {
     public private(set) var pineapple: Int = 10
     public private(set) var mango: Int = 10
 
-    /// 주문 전체과정 메서드
-    func juiceOrderProcess(orderJuice: TypesOfFruitJuice) {
-        
-        checkStockOfFruit()
-        makeFruitJuice(orderJuice)
-        checkStockOfFruit()
-    }
     
-    /// 과일재고파악 메서드
-    func checkStockOfFruit() {
+    /// 과일재고프린트 메서드
+    func printStockOfFruit() {
         print("""
                     *과일재고현황*
                     딸기: \(strawberry)
@@ -52,60 +46,6 @@ class JuiceMaker {
                     """)
     }
     
-    /// 과일쥬스 만들기 메서드
-    func makeFruitJuice(_ OrderedJuice: TypesOfFruitJuice) {
-        
-        switch OrderedJuice {
-        case .strawberryJuice:
-            if strawberry >= 16 {
-                strawberry -= 16
-                print("\(OrderedJuice) 나왔습니다.")
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        case .bananaJuice:
-            if banana >= 2 {
-                banana -= 2
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        case .kiwiJuice:
-            if kiwi >= 3 {
-                kiwi -= 3
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        case .pineappleJuice:
-            if pineapple >= 2 {
-                pineapple -= 2
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        case .strawberryAndBananaJuice:
-            if strawberry >= 10 && banana >= 1 {
-                strawberry -= 10
-                banana -= 1
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        case .mangoJuice:
-            if mango >= 3 {
-                mango -= 3
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        case .mangoAndKiwiJuice:
-            if mango >= 2 && kiwi >= 1 {
-                mango -= 2
-                kiwi -= 1
-            } else {
-                print("\(OrderedJuice)를 만들 재고가 부족합니다. 재고정리 좀 하세요")
-            }
-        default:
-            print("잘못된 주문입니다.")
-        }
-    }
-
     /// 과일재고 1개 추가 메서드
     func addStockOfFruit(addFruit: TypesOfFruit) {
         
@@ -123,8 +63,115 @@ class JuiceMaker {
         default:
             print("쓸모없는 과일입니다")
         }
+        print("\(addFruit)가 추가되었습니다.")
     }
     
+    /// 만들 수 있는지 재고 체크하는 메서드
+    func checkStockOfFruit(checkFruit: TypesOfFruitJuice) -> Bool {
+        
+        switch checkFruit {
+        case .strawberryJuice:
+            if strawberry >= 16 {
+                return true
+            } else {
+                return false
+            }
+        case .bananaJuice:
+            if banana >= 2 {
+                return true
+            } else {
+                return false
+            }
+        case .kiwiJuice:
+            if kiwi >= 3 {
+                return true
+            } else {
+                return false
+            }
+        case .pineappleJuice:
+            if pineapple >= 2 {
+                return true
+            } else {
+                return false
+            }
+        case .strawberryAndBananaJuice:
+            if strawberry >= 10 && banana >= 1 {
+                return true
+            } else {
+                return false
+            }
+        case .mangoJuice:
+            if mango >= 3 {
+                return true
+            } else {
+                return false
+            }
+        case .mangoAndKiwiJuice:
+            if mango >= 2 && kiwi >= 1 {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    
+    /// 과일재고감소 메서드
+    func removeStockOfFruit(checkFruit: TypesOfFruit, numberOfFruit: Int) {
+        
+        switch checkFruit {
+        case .strawberry:
+            strawberry -= numberOfFruit
+        case .banana:
+            banana -= numberOfFruit
+        case .kiwi:
+            kiwi -= numberOfFruit
+        case .pineapple:
+            pineapple -= numberOfFruit
+        case .mango:
+            mango -= numberOfFruit
+        default:
+            print("쓸모없는 과일입니다")
+        }
+    }
+        
+        /// 과일쥬스 만들기 메서드
+    func makeFruitJuice(_ OrderedJuice: TypesOfFruitJuice) {
+        
+        switch OrderedJuice {
+        case .strawberryJuice:
+            removeStockOfFruit(checkFruit: .strawberry, numberOfFruit: 16)
+        case .bananaJuice:
+            removeStockOfFruit(checkFruit: .banana, numberOfFruit: 2)
+        case .kiwiJuice:
+            removeStockOfFruit(checkFruit: .kiwi, numberOfFruit: 3)
+        case .pineappleJuice:
+            removeStockOfFruit(checkFruit: .pineapple, numberOfFruit: 2)
+        case .strawberryAndBananaJuice:
+            // 미해결
+            removeStockOfFruit(checkFruit: .strawberry, numberOfFruit: 16)
+        case .mangoJuice:
+            removeStockOfFruit(checkFruit: .mango, numberOfFruit: 3)
+        case .mangoAndKiwiJuice:
+            // 미해결
+            removeStockOfFruit(checkFruit: .strawberry, numberOfFruit: 16)
+        default:
+            print("잘못된 주문입니다.")
+        }
+    }
+        
+    /// 주문 전체과정 메서드
+    func juiceOrderProcess(orderJuice: TypesOfFruitJuice) {
+        
+        var wantToKnowMake: Bool = checkStockOfFruit(checkFruit: orderJuice)
+        
+        switch wantToKnowMake {
+        case true:
+            makeFruitJuice(orderJuice)
+        case false:
+            print("\(orderJuice)를 만들 재고가 부족하거나 잘못된 주문입니다.")
+            
+        }
+    }
 }
-
-JuiceMaker().juiceOrderProcess(orderJuice: .strawberryJuice)
+        
+JuiceMaker().juiceOrderProcess(orderJuice: .bananaJuice)
