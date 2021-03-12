ebff3531a2469e84cb05919b7c40ee36a07454e2
kio <un19451024@gmail.com>
2021-03-13 01:45:05 +0900

    [Feat] 재고 추가 버튼 터치 시 재고수정화면으로 이동

---
+++ JuiceMaker/JuiceMaker/StockMangerViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StockMangerViewController.swift
+//  JuiceMaker
+//
+//  Created by kio on 2021/03/13.
+//
+
+import UIKit
+
+class StockMangerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpDismisButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
