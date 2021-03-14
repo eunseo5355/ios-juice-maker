31b40b262d05c4c218eacbb41f1f95b30384ac18
kio <un19451024@gmail.com>
2021-03-15 02:15:38 +0900

    [Feat] label.text TEST, alert TEST

--- JuiceMaker/JuiceMaker/ViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -8,11 +8,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var strawberryStock: UILabel!
+    @IBOutlet weak var bananaStock: UILabel!
+    @IBOutlet weak var fineappleStock: UILabel!
+    @IBOutlet weak var kiwiStock: UILabel!
+    @IBOutlet weak var mangoStock: UILabel!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let a = JuiceMaker().fruitInformation
+        
+        strawberryStock.text = String("2")
+        bananaStock.text = String("2")
+        fineappleStock.text = String("3")
+        kiwiStock.text = String("4")
+        mangoStock.text = String("5")
     }
 
-
+    @IBAction func strawberryJuice(_ sender: Any) {
+        let alert = UIAlertController(title: "주문 성공", message: "\(JuiceTypes.strawberryJuice.rawValue) 나왔습니다! 맛있게 드세요!", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        
+        present(alert, animated: false, completion: nil)
+    }
+    
 }
 
